ff57ec1aee3aff1d0cad6c183a59238d84801b6e
todoom1 <aa13976383@outlook.com>
2026-01-10 23:47:30 +0800

    Add files via upload

---
+++ list.js
@@ -0,0 +1,25 @@
+document.getElementById("yh").addEventListener("click", function() {
+  window.location.href = "https://gfvgx.jgqvji.com/app/register.php?site_id=800&pt=F89A964F-9E2D-97D4-D6A0-45EC35392DFF";
+});
+document.getElementById("ff").addEventListener("click", function() {
+  window.location.href = "https://lncqc.jgqvji.com/app/register.php?site_id=1050&topId=7551763483266926&selfPlanId=174525";
+});
+document.getElementById("zt").addEventListener("click", function() {
+  window.location.href = "https://pkajim.hfjcl.com/app/register.php?site_id=1031&pt=66FB18D7-B58E-9A48-0B4C-079F06F2B555";
+});
+document.getElementById('st').addEventListener('click', function() {
+  window.location.href = "https://lejva.tprsmi.com/app/register.php?site_id=1058&topId=338101&selfPlanId=344542";
+});
+
+document.getElementById("0").innerHTML="🔥十年信誉平台推荐";
+
+document.getElementById("1").innerHTML="壹号娱乐";
+
+document.getElementById("5").innerHTML="非凡娱乐";
+
+document.getElementById("6").innerHTML="征途国际";
+
+document.getElementById("11").innerHTML="胜天国际";
+
+document.getElementById("57").innerHTML="ttn9.cc";
+
